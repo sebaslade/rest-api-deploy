297fc98f1eb0c791a8e99e240f7356eea5f0feea
sebaslade <sebaslade29@gmail.com>
2024-02-28 23:41:08 -0500

    Implementando Arquitectura MVC

--- app.js
+++ app.js
@@ -1,111 +1,24 @@
-const express = require('express') // require -> commonJS 
-const crypto = require('node:crypto')
-const cors = require('cors')
+import express, { json } from 'express' // require -> commonJS 
+import { moviesRouter } from './routes/movies-routes.js'
+import { corsMiddleware } from './middlewares/cors.js'
 
-const movies = require('./movies.json')
-const { validateMovie, validatePartialMovie } = require('./schemas/movies')
+//EN EL FUTURO EL IMPORT DEL JSON SERA ASÍ:
+//import movies from './movies.json' with {type: module}
+/*
+Como leer un json en ESModules
+import fs from = 'node:fs'
+const movies = JSON.parse(fs.readFileSync('./movies.json', 'utf-8'))
+*/
+
+// Como leer un json en ESModules recomendado por ahora: 
 
 const app = express()
-app.use(express.json())
-app.use(cors({
-    origin: (origin, callback) =>{
-        const ACCEPTED_ORIGINS = [
-            'http://localhost:8080',
-            'http://localhost:1234',
-            'http://movies.com'
-        ]
-        if (ACCEPTED_ORIGINS.includes(origin)) {
-            return callback(null, true)
-        }
-        if(!origin){
-            return callback(null, true)
-        }
+app.use(json())
+app.use(corsMiddleware())
+app.disable('x-powered-by')
 
-        return callback(new Error('Not allowed by CORS'))
-    }
-}))
-app.disable('x-powered-by') // deshabilitar el header X-Powered-By: Express
-
-// métodos normales: GET/HEAD/POST
-// métodos complejos: PUT/PATCH/DELETE
-
-// CORS Pre-Flight
-// OPTIONS
-
-// Todos los recursos que sean MOVIES se identifica con /movies 
-app.get('/movies', (req, res) => {
-    const { genre } = req.query
-    if (genre) {
-        const filteredMovies = movies.filter(
-            movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
-        )
-        return res.json(filteredMovies)
-    }
-    res.json(movies)
-})
-
-app.get('/movies/:id', (req, res) => { // path-to-reqexp
-    const { id } = req.params
-    const movie = movies.find(movie => movie.id === id)
-    if (movie) return res.json(movie)
-    res.status(404).json({ message: 'Movie not found' })
-})
-
-app.post('/movies', (req, res) => {
-    const result = validateMovie(req.body)
-
-    if (!result.success) {
-        //422 Unprocessable Entity
-        return res.status(400).json({ error: JSON.parse(result.error.message) })
-    }
-    const newMovie = {
-        id: crypto.randomUUID(), // uuid v4
-        ...result.data
-    }
-
-    // Esto no sería REST, porque estamos guardando
-    // el estado de la aplicación en memoria
-    movies.push(newMovie)
-
-    res.status(201).json(newMovie)
-})
-
-app.delete('/movies/:id', (req, res) => {
-    const { id } = req.params
-    const movieIndex = movies.findIndex(movie => movie.id === id)
-
-    if (movieIndex === -1) {
-        return res.status(404).json({ message: 'Movie not found' })
-    }
-
-    movies.splice(movieIndex, 1)
-
-    return res.json({ message: 'Movie deleted' })
-})
-
-app.patch('/movies/:id', (req, res) => {
-    const result = validatePartialMovie(req.body)
-
-    if (!result.success) {
-        return res.status(400).json({ error: JSON.parse(result.error.message) })
-    }
-
-    const { id } = req.params
-    const movieIndex = movies.findIndex(movie => movie.id === id)
-
-    if (movieIndex === -1) {
-        return res.status(404).json({ message: 'Movie not found' })
-    }
-
-    const updateMovie = {
-        ...movies[movieIndex],
-        ...result.data
-    }
-
-    movies[movieIndex] = updateMovie
-
-    return res.json(updateMovie)
-})
+// Todas las rutas que estan en moviesRouter
+app.use('/movies', moviesRouter)
 
 const PORT = process.env.PORT ?? 1234
 
---
+++ models/movie-model.js
@@ -0,0 +1,50 @@
+import { randomUUID } from 'node:crypto'
+import { readJSON } from '../utils.js'
+
+const movies = readJSON('../movies.json')
+
+export class MovieModel {
+    static async getAll({ genre }) {
+        if (genre) {
+            // Como se filtran los datos y de dónde se recuperan
+            const filteredMovies = movies.filter(
+                movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+            )
+            return res.json(filteredMovies)
+        }
+        res.json(movies)
+    }
+    static async getById({ id }) {
+        const movie = movies.find(movie => movie.id === id)
+        return movie
+    }
+    static async create({ input }) {
+        const newMovie = {
+            id: randomUUID(), // uuid v4
+            ...input
+        }
+
+        // Esto no sería REST, porque estamos guardando
+        // el estado de la aplicación en memoria
+        movies.push(newMovie)
+
+        return newMovie
+    }
+    static async delete({ id }) {
+        const movieIndex = movies.findIndex(movie => movie.id === id)
+
+        if (movieIndex === -1) return false
+
+        movies.splice(movieIndex, 1)
+        return true
+    }
+    static async update({ id, input }){
+        const movieIndex = movies.findIndex(movie => movie.id === id)
+        
+        movies[movieIndex]={
+            ...movies[movieIndex],
+            ...input
+        }
+        return movies[movieIndex]
+    }
+}

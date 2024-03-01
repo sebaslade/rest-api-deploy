91b3520c32acefe0b96e7c857d959e5451cdb288
sebaslade <sebaslade29@gmail.com>
2024-03-01 18:47:35 -0500

    Agregando el link de la API

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,8 @@ import cors from 'cors'
 const ACCEPTED_ORIGINS = [
     'http://localhost:8080',
     'http://localhost:1234',
-    'http://movies.com'
+    'http://movies.com',
+    'https://rest-api-deploy-dev-hgdf.3.us-1.fl0.io/movies'
 ]
 
 export const corsMiddleware = ({acceptedOrigins = ACCEPTED_ORIGINS} = {}) => cors({
